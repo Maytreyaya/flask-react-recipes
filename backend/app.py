3db35a5bcc7c26a46a01d44ce97353605c8aa9a4
Maksym Nikityuk <maksym71417@gmail.com>
2023-09-29 12:59:17 +0300

    config, models, exts files created, first CRUD operations created in app.py

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,86 @@
-from flask import Flask
+from flask import Flask, request
+from flask_restx import Api, Resource, fields
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import requests
+
+from config import DevConfig
+from models import Recipe
+from exts import db
 
 app = Flask(__name__)
+app.config.from_object(DevConfig)
+
+db.init_app(app)
+
+migrate = Migrate(app, db)
+
+api = Api(app, doc="/docs")
+
+recipe_model = api.model(
+    "Recipe",
+    {
+        "id": fields.Integer,
+        "title": fields.String,
+        "description": fields.String,
+    }
+)
+
+
+@api.route("/hello")
+class Hello(Resource):
+    def get(self):
+        return {"message": "hello_world"}
+
+
+@api.route("/recipes")
+class RecipesResource(Resource):
+    @api.marshal_list_with(recipe_model)
+    def get(self):
+        """Get all recipes"""
+        recipes = Recipe.query.all()
+
+        return recipes
+
+    @api.marshal_with(recipe_model, envelope="resource")
+    def post(self):
+        """Create a new recipe"""
+        data = request.get_json()
+        if not data:
+            return {"message": "Invalid JSON data"}, 400
+        new_recipe = Recipe(title=data.get("title"),
+                            description=data.get("description"),
+                            )
+        new_recipe.save()
+
+        return new_recipe, 201
+
+
+@api.route("/recipe/<int:id>/")
+class RecipeResource(Resource):
+    @api.marshal_with(recipe_model)
+    def get(self, id: int):
+        result = Recipe.query.get_or_404(id)
+        return result
+
+    @api.marshal_with(recipe_model)
+    def put(self, id: int):
+        """Update a recipe by id"""
+        recipe_to_upd = Recipe.query.get_or_404(id)
+        data = request.get_json()
+
+        recipe_to_upd.update(data.get("title"),
+                             data.get("description"))
+        return recipe_to_upd
+
+    @api.marshal_with(recipe_model)
+    def delete(self, id: int):
+        """Delete recipe by id"""
+        recipe_to_del = Recipe.query.get_or_404(id)
+        recipe_to_del.delete()
+
+        return recipe_to_del
+        pass
 
 
 @app.route('/')
---
+++ backend/models.py
@@ -0,0 +1,24 @@
+from exts import db
+
+
+class Recipe(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(), nullable=False)
+    description = db.Column(db.Text(), nullable=False)
+
+    def __repr__(self):
+        return f"<Recipe {self.title}>"
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self, title: str, description: str):
+        self.title = title
+        self.description = description
+
+        db.session.commit()
