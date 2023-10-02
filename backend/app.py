cecb826fb1483fb9f2346c3970bbbafec1359f17
Maksym Nikityuk <maksym71417@gmail.com>
2023-10-02 18:57:58 +0300

    SigUp and Login functionality with JWT implemented

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,14 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_restx import Api, Resource, fields
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-import requests
-
 from config import DevConfig
-from models import Recipe
+from models import Recipe, User
 from exts import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import (JWTManager,
+                                create_access_token,
+                                create_refresh_token,
+                                jwt_required)
 
 app = Flask(__name__)
 app.config.from_object(DevConfig)
@@ -14,6 +16,8 @@ app.config.from_object(DevConfig)
 db.init_app(app)
 
 migrate = Migrate(app, db)
+
+JWTManager(app)
 
 api = Api(app, doc="/docs")
 
@@ -26,11 +30,71 @@ recipe_model = api.model(
     }
 )
 
+signup_model = api.model(
+    "SignUp",
+    {
+        "username": fields.String,
+        "email": fields.String,
+        "password": fields.String,
+    }
+)
+
+login_model = api.model(
+    "Login",
+    {
+        "username": fields.String,
+        "password": fields.String,
+    }
+)
+
 
 @api.route("/hello")
 class Hello(Resource):
     def get(self):
         return {"message": "hello_world"}
+
+
+@api.route("/signup")
+class SignUp(Resource):
+    @api.expect(signup_model)
+    def post(self):
+        data = request.get_json()
+        username = data.get("username")
+        db_user = User.query.filter_by(username=username).first()
+
+        if db_user:
+            return jsonify({"message": f"Username {username} is taken, choose another one"}, 400)
+
+        if not data:
+            return {"message": "Invalid JSON data"}, 400
+        new_user = User(username=data.get("username"),
+                        email=data.get("email"),
+                        password=generate_password_hash(data.get("password")))
+        new_user.save()
+
+        return jsonify({"message": "User successfully created"})
+
+
+@api.route("/login")
+class Login(Resource):
+    @api.expect(login_model)
+    def post(self):
+        data = request.get_json()
+
+        username = data.get("username")
+        password = data.get("password")
+
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
+            access_token = create_access_token(identity=user.username)
+            refresh_token = create_refresh_token(identity=user.username)
+
+            return jsonify(
+                {
+                 "access_token": access_token,
+                 "refresh_token": refresh_token
+                 }
+            )
 
 
 @api.route("/recipes")
@@ -42,7 +106,9 @@ class RecipesResource(Resource):
 
         return recipes
 
-    @api.marshal_with(recipe_model, envelope="resource")
+    @api.marshal_with(recipe_model)
+    @api.expect(recipe_model)
+    @jwt_required()
     def post(self):
         """Create a new recipe"""
         data = request.get_json()
@@ -63,6 +129,7 @@ class RecipeResource(Resource):
         result = Recipe.query.get_or_404(id)
         return result
 
+    @jwt_required()
     @api.marshal_with(recipe_model)
     def put(self, id: int):
         """Update a recipe by id"""
@@ -74,6 +141,7 @@ class RecipeResource(Resource):
         return recipe_to_upd
 
     @api.marshal_with(recipe_model)
+    @jwt_required()
     def delete(self, id: int):
         """Delete recipe by id"""
         recipe_to_del = Recipe.query.get_or_404(id)
--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,5 @@
 from exts import db
+from werkzeug.security import generate_password_hash
 
 
 class Recipe(db.Model):
@@ -20,5 +21,30 @@ class Recipe(db.Model):
     def update(self, title: str, description: str):
         self.title = title
         self.description = description
+
+        db.session.commit()
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(25), nullable=False, unique=True)
+    email = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.Text(), nullable=False)
+
+    def __repr__(self):
+        return f"<User: {self.username}>"
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self, username: str, email: str, password: str):
+        self.username = username
+        self.email = email
+        self.password = generate_password_hash(password)
 
         db.session.commit()
