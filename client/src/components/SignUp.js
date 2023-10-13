a712998bed69d98f5965647ef74e40da0ed9e2db
Maksym Nikityuk <maksym71417@gmail.com>
2023-10-13 18:56:51 +0300

    React alert for success registration implemented

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,13 +1,15 @@
 import React, {useState} from "react"
 import {
     Form,
-    Button
+    Button,
+    Alert
 } from "react-bootstrap"
 import "../styles/main.css"
 import {Link} from "react-router-dom";
 import {useForm} from "react-hook-form";
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
+import data from "bootstrap/js/src/dom/data";
 
 const schema = yup.object().shape({
   username: yup.string().required('Username is required').max(25, 'Max length is 25 chars'),
@@ -17,24 +19,24 @@ const schema = yup.object().shape({
 });
 
 const SignUpPage=()=>{
-    // const [username, setUsername]=useState('')
-    // const [email, setEmail]=useState('')
-    // const [password, setPassword]=useState('')
-    // const [confirmPassword, setConfirmPassword]=useState('')
+
+    const [show, setShow] = useState(false);
+    const [serverResponse, setServerResponse]=useState('')
+    const [username, setUsername] = useState('');
+
 
     const {register,watch,reset,handleSubmit,formState:{errors}} = useForm({
           resolver: yupResolver(schema)
     });
 
     const submitForm=(data)=>{
-        console.log(data)
-
         if (data.password === data.confirmPassword) {
             const body = {
                     "username": data.username,
                     "email": data.email,
                     "password": data.password,
                 }
+                setUsername(data.username)
             const requestOptions = {
                 method: "POST",
                 headers: {
@@ -45,22 +47,41 @@ const SignUpPage=()=>{
             }
             fetch("auth/signup", requestOptions)
                 .then(res=>res.json())
-                .then(data=>console.log(data))
+                .then(data=>{
+                    console.log(data)
+                    setServerResponse(data.message)
+                    console.log(serverResponse)
+                    setShow(true)
+                })
                 .catch(err=>console.log(err))
         reset()
+            console.log(username)
+
         }
         else {
             alert("Passwords do not match!")
         }
     }
 
-    console.log(watch("username"));
+    // console.log(watch("username"));
 
 
 
     return(
         <div className="container">
             <div className="signup-form">
+                {show?
+                <Alert className="alert-class" variant="success" onClose={() => setShow(false)} dismissible>
+                    <Alert.Heading>Well done, {username}!</Alert.Heading>
+                    <p>
+                        {serverResponse}
+                        <br/>
+                        Welcome Home...
+                    </p>
+                </Alert>
+                        :
+                    <dfn><b><p>Welcome back, stranger...</p></b></dfn>
+                }
                 <form className="form" action="">
                     <h1>Sign Up</h1>
                   <Form.Group className="formmm">
@@ -71,12 +92,8 @@ const SignUpPage=()=>{
                           {...register("username",{
                               required:true,
                               maxLength:25})}
-                          // value={username}
-                          // name="username"
-                          // onChange={(e)=>{setUsername(e.target.value)}}
                       ></Form.Control>
-                      {errors.username && <span style={{color:"red"}}><small>Username is required</small></span>}
-                      {errors.username?.type==="maxLength" && <span style={{color:"red"}}> Max length is 25 chars </span>}
+                      {errors.username && <span style={{ color: "red" }}><small>{errors.username.message}</small></span>}
                   </Form.Group>
                   <Form.Group className="formmm">
                       <Form.Label>Email</Form.Label>
@@ -87,12 +104,8 @@ const SignUpPage=()=>{
                               required:true,
                               maxLength:80,
                           }, )}
-                          // value={email}
-                          // name="email"
-                          // onChange={(e)=>{setEmail(e.target.value)}}
                       ></Form.Control>
-                      {errors.email && <span style={{color:"red"}}><small>Email is required</small></span>}
-                      {errors.username?.type==="maxLength" && <span style={{color:"red"}}> Max length is 80 chars </span>}
+                      {errors.email && <span style={{ color: "red" }}><small>{errors.email.message}</small></span>}
                   </Form.Group>
                   <Form.Group className="formmm">
                       <Form.Label>Password</Form.Label>
@@ -103,12 +116,8 @@ const SignUpPage=()=>{
                               required:true,
                               minLength:8,
                           }, )}
-                          // value={password}
-                          // name="password"
-                          // onChange={(e)=>{setPassword(e.target.value)}}
                       ></Form.Control>
-                      {errors.password && <span style={{color:"red"}}><small>Password is required</small></span>}
-                      {errors.password && <span style={{color:"red"}}><small>Password must be at least 8 char long</small></span>}
+                      {errors.password && <span style={{ color: "red" }}><small>{errors.password.message}</small></span>}
                   </Form.Group>
                   <Form.Group className="formmm">
                       <Form.Label>Confirm password</Form.Label>
@@ -119,12 +128,8 @@ const SignUpPage=()=>{
                               required:true,
                               minLength:8,
                           }, )}
-                          // value={confirmPassword}
-                          // name="confirmPassword"
-                          // onChange={(e)=>{setConfirmPassword(e.target.value)}}
                       ></Form.Control>
                       {errors.confirmPassword && <span style={{ color: "red" }}><small>{errors.confirmPassword.message}</small></span>}
-
                   </Form.Group>
                   <Form.Group>
                     <Button className="acaca" as="sub" variant="primary" onClick={handleSubmit(submitForm)}>Submit</Button>
